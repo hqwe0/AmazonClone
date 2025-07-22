69239cb6e3ed22f13f7f6b20b012d32dbd51f343
GunesEnes <gunesenes9@outlook.com>
2025-07-22 11:32:33 +0300

    Create automated tests for formatCurreny

---
+++ tests/moneyTest.js
@@ -0,0 +1,27 @@
+import { formatCurreny } from '../scripts/utils/money.js';
+
+console.log('Test suite: formatCurrency');
+
+console.log('convert cents into dollars')
+if (formatCurreny(2095) === '20.95') {
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
+console.log('work with 0')
+if (formatCurreny(0) === '0.00') {
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
+console.log('rounds up nearest cent')
+if (formatCurreny(2000.5 === '20.01')) {
+  console.log('passed');
+}
+else {
+  console.log('failed');
+}
